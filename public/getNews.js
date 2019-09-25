0ac4dc902c21d3c15ad933662fffa7e4a770fb95
CN138 <cn138@CN0138.local>
2019-09-25 15:31:28 +0100

    first

---
+++ public/getNews.js
@@ -0,0 +1,9 @@
+const request = require ('request')
+const {promisify} = require('util')
+const promisifiedRequest = promisify(request)
+const{apiKey} = require('./keys')
+
+
+const getNews = () => {
+    
+}
---
+++ public/keys.js
@@ -0,0 +1,5 @@
+const apiKey = "a1f4db5674e742a0b35037b7a5a353c3"
+
+module.exports ={
+    apiKey
+}
