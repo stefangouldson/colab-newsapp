13396113e0417eedf9f6b67473b4827f6dd944e3
CN138 <cn138@CN0138.local>
2019-09-26 10:12:54 +0100

    update

--- getLocation.js
+++ getLocation.js
@@ -9,7 +9,7 @@ const getLocation = async (places) => {
         let data1 = await promisifiedRequest({url: `https://api.mapbox.com/geocoding/v5/mapbox.places/${places}.json?access_token=${pk}`, json: true})        
         return({
             // returns the shorthand country code "US"
-            country: data1.body.context[1].short_code,
+            location: data1.body.place.place.features[0].context[3].short_code,
 
         })
     } catch (error) {
--- getNews.js
+++ getNews.js
@@ -7,10 +7,10 @@ const{apiKey} = require('./keys')
 const getNews = async(location) => {
     try{
         let data = await
-    promisifiedRequest({url: `https://newsapi.org/v2/top-headlines?country=${location}s&category=${type}&apiKey=${apiKey}`,json: true})
+    promisifiedRequest({url: `https://newsapi.org/v2/top-headlines?country=${location}s&category=buissness&apiKey=${apiKey}`,json: true})
     //returns JSON string that returns evrything int the article object 
     return(data.body.articles)
- }  catch (error){
+}  catch (error){
         console.log("Something has gone wrong!")
     }
 }
