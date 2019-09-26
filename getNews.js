e746030a82dc8664718c54d97fe72d9a5193e210
CN138 <cn138@CN0138.local>
2019-09-26 08:48:24 +0100

    update

--- getNews.js
+++ getNews.js
@@ -3,11 +3,12 @@ const {promisify} = require('util')
 const promisifiedRequest = promisify(request)
 const{apiKey} = require('./keys')
 
-// get news from api, need to add country and type of news later.
+// get news from api, need to add type of news later.
 const getNews = async(location) => {
     try{
         let data = await
     promisifiedRequest({url: `https://newsapi.org/v2/top-headlines?country=${location}s&category=${type}&apiKey=${apiKey}`,json: true})
+    //returns JSON string that 
     return(data.body.articles)
  }  catch (error){
         console.log("Something has gone wrong!")
