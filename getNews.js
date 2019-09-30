6122a2c59db92bb7670566d50949ccbf1834300c
CN138 <cn138@CN0138.local>
2019-09-30 15:46:47 +0100

    update

--- getNews.js
+++ getNews.js
@@ -2,7 +2,8 @@ const request = require ('request')
 const {promisify} = require('util')
 const promisifiedRequest = promisify(request)
 const{apiKey} = require('./keys')
-const random = Math.floor(Math.random() * 10)
+
+
 
 // get news from api, need to add type of news later.
 const getNews = async(location, cat) => {
@@ -11,11 +12,16 @@ const getNews = async(location, cat) => {
     promisifiedRequest({url: `https://newsapi.org/v2/top-headlines?country=${location}&category=${cat}&apiKey=${apiKey}`, json: true})
     //returns JSON string that returns evrything int the article object
     // console.log(data.body.articles[0])
+    
+        let random = Math.floor(Math.random() * 10);
+      
+
     return {
         body: data.body.articles[random],
         title: data.body.articles[random].title,
         content: data.body.articles[random].description,
-        image: data.body.articles[random].urlToImage}
+        image: data.body.articles[random].urlToImage
+    } 
 }  catch (error){
         console.log("Something has gone wrong!")
     }
