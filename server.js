68623e02cad40d92f64b41245003d8e9c1953755
CN0103 <cn0103@CN103.local>
2019-09-30 11:59:12 +0100

    button styles

--- server.js
+++ server.js
@@ -7,18 +7,9 @@ const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/news", async (req, res)=>{
-<<<<<<< HEAD
    let data1 = await main(req.query.location);
    // let data2 = await main(req.query.type);
    res.send(data1);
-=======
-   // let data1 = await main(req.query.location);
-   // // let data2 = await main(req.query.type);
-   // res.send(data1);
-   let data1 = getNews(req.query.location,req.query.category )
-   res.send(data1)
-
->>>>>>> 89a625aa40912b63ec385a515e9d58ecaa28a05b
 });
 app.listen(3006, () => {
    console.log("listening to port 3006");
